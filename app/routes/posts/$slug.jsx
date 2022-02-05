1087fe2776a1ef743a075820c7d2561e4d9f60e3
Peter Colapietro <petercolapietro+spacehippie@gmail.com>
2022-02-05 13:06:40 -0500

    init: 🚀

---
+++ app/routes/posts/$slug.jsx
@@ -0,0 +1,17 @@
+import { useLoaderData } from "remix";
+import { getPost } from "~/post";
+import invariant from "tiny-invariant";
+
+export const loader = async ({ params }) => {
+  invariant(params.slug, "expected params.slug");
+  return getPost(params.slug);
+};
+
+export default function PostSlug() {
+  const post = useLoaderData();
+  return (
+    <div>
+      <div dangerouslySetInnerHTML={{ __html: post.html }} />
+    </div>
+  );
+}
