28d611a89c2cfd4ed0c2a7361d1e5f738e39bb22
Peter Colapietro <petercolapietro+spacehippie@gmail.com>
2023-09-25 22:01:43 -0400

    chore: cleanup
    https://remix.run/docs/en/1.15.0/pages/v2#file-system-route-convention

--- remix.config.js
+++ remix.config.js
@@ -1,3 +1,5 @@
+const { createRoutesFromFolders } = require("@remix-run/v1-route-convention");
+
 /**
  * @type {import('@remix-run/dev/config').AppConfig}
  */
@@ -18,5 +20,9 @@ module.exports = {
     v2_dev: {
       port: 8002,
     },
+  },
+  routes(defineRoutes) {
+    // uses the v1 convention, works in v1.15+ and v2
+    return createRoutesFromFolders(defineRoutes);
   },
 };
